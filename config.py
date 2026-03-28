5269c74950b9e89210a5d376413a98d09b4b67d0
Mark Anjoul <marc.anjoul@gmail.com>
2026-03-28 12:27:54 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+state = {
+    "ear": 0.31,
+    "alert_stage": 0,
+    "alarm_silenced": False,
+    "yawn_count": 0,
+    "session_active": False,
+}
---
+++ ui.py
@@ -0,0 +1,6 @@
+import customtkinter as ctk
+import cv2
+from PIL import Image, ImageTk
+
+from config import state
+
